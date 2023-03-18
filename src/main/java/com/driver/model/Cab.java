cf321aceea36973164497fabc2fd26b526a626db
Ankit Litouriya <ankitlitouriya8719@gmail.com>
2023-03-18 23:54:23 +0530

    second

--- src/main/java/com/driver/model/Cab.java
+++ src/main/java/com/driver/model/Cab.java
@@ -1,29 +1,66 @@
 package com.driver.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import javax.persistence.*;
 
 @Entity
-
-@AllArgsConstructor
-@NoArgsConstructor
- @Getter
-@Setter
-@Table(name = "cab")
+@Table
 public class Cab {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+
     private int perKmRate;
+
     private boolean available;
 
-    @OneToOne
-    @JoinColumn
+    public Cab() {
+    }
+
+    public Cab(int perKmRate) {
+        this.perKmRate = perKmRate;
+    }
+
+    public Cab(int id, int perKmRate, boolean available) {
+        this.id = id;
+        this.perKmRate = perKmRate;
+        this.available = available;
+    }
+
+
+
+    @OneToOne(mappedBy = "cab", cascade = CascadeType.ALL)
     private Driver driver;
 
+    public Driver getDriver() {
+        return driver;
+    }
 
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPerKmRate() {
+        return perKmRate;
+    }
+
+    public void setPerKmRate(int perKmRate) {
+        this.perKmRate = perKmRate;
+    }
+
+    public boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
 }
